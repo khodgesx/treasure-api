fa48feaedbdad7c53c917f2ee9b778a14970de0d
khodges <zkellyhodges@gmail.com>
2022-04-12 10:55:42 -0700

    took out image stuff to keep this at mvp

--- items_api/serializers.py
+++ items_api/serializers.py
@@ -4,4 +4,5 @@ from .models import Item
 class ItemSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Item # tell django which model to use
-        fields = ('id', 'title', 'category', 'details', 'amount', 'img', 'location', 'available',) # tell django which fields to include
+        fields = ('id', 'title', 'category', 'details', 'amount', 'location', 'available',) # tell django which fields to include
+#'img',
