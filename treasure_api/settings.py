63846a27d529ced60764e9d60eb726916964ebff
khodges <zkellyhodges@gmail.com>
2022-04-15 09:10:36 -0700

    added debug false

--- treasure_api/settings.py
+++ treasure_api/settings.py
@@ -158,7 +158,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # #user model
 # AUTH_USER_MODEL = 'users.CustomUser'
